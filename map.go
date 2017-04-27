d26dbfdfa67071d2bf299560c91aa98e88d01b5f
Artur Grabowski <art@blahonga.org>
2017-04-27 17:23:10 +0000

    Icon instead of text.
    Icon from material.io.

--- map.go
+++ map.go
@@ -9,6 +9,12 @@ var _ = tmpls.Add("map", `
 {{template "header"}}
 <div style="width: 95%; height: 95%">
 	<svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="100%" height="100%" viewBox="0 0 24 20" id="themap">
+		<defs>
+		  <g id="sunny">
+		    <path d="M0 0h24v24H0z" fill="none"/>
+		    <path d="M6.76 4.84l-1.8-1.79-1.41 1.41 1.79 1.79 1.42-1.41zM4 10.5H1v2h3v-2zm9-9.95h-2V3.5h2V.55zm7.45 3.91l-1.41-1.41-1.79 1.79 1.41 1.41 1.79-1.79zm-3.21 13.7l1.79 1.8 1.41-1.41-1.8-1.79-1.4 1.4zM20 10.5v2h3v-2h-3zm-8-5c-3.31 0-6 2.69-6 6s2.69 6 6 6 6-2.69 6-6-2.69-6-6-6zm-1 16.95h2V19.5h-2v2.95zm-7.45-3.91l1.41 1.41 1.79-1.8-1.41-1.41-1.79 1.8z"/>
+		  </g>
+		</defs>
 		<style>
 			.s {
 				width: 0.8;
@@ -135,7 +141,9 @@ var _ = tmpls.Add("map-sector", `
 {{- range $i, $row := (sectorName .)}}
     <text transform="translate(0 {{$i}})" class="sectorname">{{$row}}</text>
 {{- end}}
-    <text transform="translate(0 3)" class="sectordesc">Suns: {{.SunPercent}}</text>
+{{if gt .SunPercent 150}}
+    <use transform="translate(0 4) scale(0.04)" xlink:href="#sunny" />
+{{end}}
   </g>
  </g>
  <rect class="s sectorhover" />
