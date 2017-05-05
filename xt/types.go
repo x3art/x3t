eeff41341ef3f8f1d159cf545bd42fdfee45ddde
Artur Grabowski <art@blahonga.org>
2017-05-05 19:07:46 +0000

    Slam together all the types files into one.
    Eventually, I 'll decide against it and split it up again, but right now things were becoming silly.

--- xt/types.go
+++ xt/types.go
@@ -1,5 +1,37 @@
 package xt
 
+func (x *X) GetSuns() []TSun {
+	x.sunsOnce.Do(func() {
+		f := x.xf.Open("addon/types/TSuns.txt")
+		defer f.Close()
+		tparse(f, x.GetText(), &x.suns)
+	})
+	return x.suns
+}
+
+// the only documentation of this I found was wrong.
+type TSun struct {
+	Unknown01  int
+	Unknown02  int
+	Unknown03  int
+	Unknown04  int
+	Unknown05  float64
+	Unknown06  int
+	Unknown07  int
+	Unknown08  int
+	Unknown09  int
+	Brightness int // Seems to be fixed point scaled by 2^16
+	Unknown11  int
+	Unknown12  int
+	Unknown13  int
+	Unknown14  int
+	Unknown15  int
+	Unknown16  int
+	Unknown17  int
+	Unknown18  int
+	ObjectID   string
+}
+
 func (x *X) GetShips() []Ship {
 	x.shipsOnce.Do(func() {
 		f := x.xf.Open("addon/types/TShips.txt")
@@ -132,4 +164,114 @@ type Ship struct {
 	UknownValue string
 	// Ship ID - identifier of the ship
 	ShipID string
+}
+
+func GetCockpits(xf Xfiles, text Text) []Cockpit {
+	f := xf.Open("addon/types/TCockpits.txt")
+	defer f.Close()
+	ret := []Cockpit{}
+	tparse(f, text, &ret)
+	return ret
+}
+
+type Cockpit struct {
+	BodyFile               string
+	PictureID              string
+	RotX                   float64
+	RotY                   float64
+	RotZ                   float64
+	GalaxySubtype          string
+	Description            string
+	SceneFile              string
+	LaserMask              int
+	Volume                 string
+	ProductionRelValNPC    string
+	PriceModifier1         string
+	PriceModifier2         string
+	WareClass              string
+	ProductionRelValPlayer string
+	MinNotoriety           string
+	VideoID                string
+	Skin                   string
+	ObjectID               string
+}
+
+func (x *X) GetDocks() map[string]TDock {
+	x.docksOnce.Do(func() {
+		f := x.xf.Open("addon/types/TDocks.txt")
+		defer f.Close()
+		ds := []TDock{}
+		tparse(f, x.GetText(), &ds)
+		x.docks = make(map[string]TDock)
+		for _, d := range ds {
+			x.docks[d.ObjectID] = d
+		}
+	})
+
+	return x.docks
+}
+
+type TDock struct {
+	BodyFile               string
+	PictureID              string
+	RotX                   float64
+	RotY                   float64
+	RotZ                   float64
+	GalaxySubtype          string
+	Description            string `x3t:"page:17"`
+	SoundID                string
+	DockDistance           string
+	RendezvousDistrance    string
+	ThreeDSoundVolume      string
+	SceneFile              string
+	InnerScene             string
+	Race                   int
+	Explosion              string
+	BodyExplosion          string
+	ShieldPowerGen         string
+	HUDIcon                string
+	Volume                 string
+	ProductionRelValNPC    int
+	PriceModifier1         int
+	PriseModifier2         int
+	WareClass              string
+	ProductionRelValPlayer int
+	MinNotoriety           int
+	VideoID                string
+	Skin                   string
+	ObjectID               string
+}
+
+func GetLasers(xf Xfiles, text Text) []Laser {
+	f := xf.Open("addon/types/TLasers.txt")
+	defer f.Close()
+	ret := []Laser{}
+	tparse(f, text, &ret)
+	return ret
+}
+
+type Laser struct {
+	BodyFile               string
+	PictureID              string
+	RotX                   float64
+	RotY                   float64
+	RotZ                   float64
+	Index                  string
+	Description            string `x3t:"page:17"`
+	RoF                    int
+	Sound                  int
+	Projectile             int
+	Energy                 int
+	ChargeRate             float64
+	HUDIcon                string
+	Volume                 string
+	ProductionRelValNPC    string
+	PriceMod1              string
+	PriceMod2              string
+	WareClass              string
+	ProductionRelValPlayer string
+	MinNotoriety           string
+	VideoID                string
+	Skin                   string
+	OjectID                string
 }
