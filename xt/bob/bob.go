caf5d255befee3458901579c997a2a2bdb331b60
Artur Grabowski <art@blahonga.org>
2017-09-27 18:54:56 +0000

    Decode much more.
    The object I'm testing with is now fully decoded.

--- xt/bob/bob.go
+++ xt/bob/bob.go
@@ -9,15 +9,17 @@ import (
 	"reflect"
 )
 
-// Current understanding of the BOB format:
-// Each section has an opening and a closing tag.
-// The whole file is a section that starts with BOB1 and ends with \BOB
+/*
+ * Whoever designed this "binary" format should take a hard look at
+ * himself in the mirror. Mixing 32 bit and 16 bit array sizes and
+ * special casing types we decode to by flags...
+ */
 
 func Read(r io.Reader) {
 	br := bufio.NewReader(r)
 	b := bob{}
 	err := sect(br, "BOB1", "/BOB", false, func() error { return decodeVal(br, 0, &b) })
-	fmt.Printf("%v\n", b)
+	//	fmt.Printf("%v\n", b)
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -25,32 +27,40 @@ func Read(r io.Reader) {
 }
 
 func sect(r *bufio.Reader, s, e string, optional bool, f func() error) error {
-	hdr := make([]byte, 4, 4)
-	_, err := r.Read(hdr)
+	hdr, err := r.Peek(4)
 	if err != nil {
 		return err
 	}
 	if string(hdr) != s {
 		if optional {
+			log.Printf("unoptional(%s) %v/%s", s, hdr, hdr)
 			return nil
 		}
 		return fmt.Errorf("unexpected [%s], expected [%s]", hdr, s)
+	} else {
+		_, err = r.Read(hdr)
+		if err != nil {
+			return err
+		}
 	}
+	log.Printf("start %s", s)
 	err = f()
 	if err != nil {
 		return err
 	}
 	_, err = r.Read(hdr)
 	if string(hdr) != e {
-		return fmt.Errorf("unexpected [%s], expected [%s]", hdr, e)
+		log.Print(r.Peek(100))
+		return fmt.Errorf("unexpected [%s]%v, expected [%s]", hdr, hdr, e)
 	}
+	log.Printf("end %s", s)
 	return nil
 }
 
 type bob struct {
-	Info info
-	Mat6 mat6
-	Body body
+	Info   info
+	Mat6   mat6
+	Bodies bodies
 }
 
 type info string
@@ -239,7 +249,131 @@ func (m *mat6) Decode(r *bufio.Reader) error {
 	return sect(r, "MAT6", "/MAT", false, func() error { return decodeVal(r, skipMethod|len32, m) })
 }
 
+type bones []string
+
+func (b *bones) Decode(r *bufio.Reader) error {
+	return sect(r, "BONE", "/BON", true, func() error { return decodeVal(r, skipMethod|len32, b) })
+}
+
+type point struct {
+	hdr struct {
+		Type int16
+	}
+	values []int32
+}
+
+func (p *point) Decode(r *bufio.Reader) error {
+	err := decodeVal(r, 0, &p.hdr)
+	if err != nil {
+		return err
+	}
+	sz := 0
+	switch p.hdr.Type {
+	case 0x1f:
+		sz = 11
+	case 0x1b:
+		sz = 9
+	case 0x19:
+		sz = 7
+	default:
+		return fmt.Errorf("unknown point type %d", p.hdr.Type)
+	}
+	p.values = make([]int32, sz)
+	for i := range p.values {
+		err := decodeVal(r, 0, &p.values[i])
+		if err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+type points []point
+
+func (p *points) Decode(r *bufio.Reader) error {
+	return sect(r, "POIN", "/POI", true, func() error { return decodeVal(r, skipMethod|len32, p) })
+}
+
+type weight struct {
+	Weights []struct {
+		Idx   int16
+		Coeff int32
+	}
+}
+
+type weights []weight
+
+func (w *weights) Decode(r *bufio.Reader) error {
+	return sect(r, "WEIG", "/WEI", true, func() error { return decodeVal(r, skipMethod|len32, w) })
+}
+
+type uv struct {
+	Idx    int32
+	Values [6]float32
+}
+
+type uvlist []uv
+
+func (u *uvlist) Decode(r *bufio.Reader) error {
+	return decodeVal(r, skipMethod|len32, u)
+}
+
+type faces [][4]int32
+
+func (f *faces) Decode(r *bufio.Reader) error {
+	return decodeVal(r, skipMethod|len32, f)
+}
+
+type faceList struct {
+	MaterialIndex int32
+	Faces         faces
+}
+
+type faceListX3 struct {
+	MaterialIndex int32
+	Faces         faces
+	UVList        uvlist
+}
+
+type part struct {
+	Hdr struct {
+		Flags int32
+	}
+	x3 struct {
+		FacesX3 []faceListX3
+		X3Vals  [10]int32
+	}
+	notx3 struct {
+		Faces []faceList
+	}
+}
+
+func (p *part) Decode(r *bufio.Reader) error {
+	err := decodeVal(r, 0, &p.Hdr)
+	if err != nil {
+		return err
+	}
+	if (p.Hdr.Flags & 0x10000000) != 0 {
+		err = decodeVal(r, 0, &p.x3)
+	} else {
+		err = decodeVal(r, 0, &p.notx3)
+	}
+	return err
+}
+
+type parts []part
+
+func (p *parts) Decode(r *bufio.Reader) error {
+	return sect(r, "PART", "/PAR", true, func() error { return decodeVal(r, skipMethod|len32, p) })
+}
+
 type body struct {
+	Size    int32
+	Flags   int32
+	Bones   bones
+	Points  points
+	Weights weights
+	Parts   parts
 	/*
 		bones // bone
 		points // POINT section?
@@ -249,5 +383,13 @@ type body struct {
 }
 
 func (b *body) Decode(r *bufio.Reader) error {
+	err := decodeVal(r, skipMethod, b)
+	fmt.Printf("body %x\n", b.Size)
+	return err
+}
+
+type bodies []body
+
+func (b *bodies) Decode(r *bufio.Reader) error {
 	return sect(r, "BODY", "/BOD", false, func() error { return decodeVal(r, skipMethod, b) })
 }
